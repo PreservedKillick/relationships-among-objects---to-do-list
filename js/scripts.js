0cc9d24a31a6f5c450ee2fe14fcec0de9c017ddd
Daniel Miller & Angela Yen <student@epicodus.com>
2014-02-25 16:58:52 -0800

    relationships btw objects, to-do list

--- js/scripts.js
+++ js/scripts.js
@@ -1,150 +1,51 @@
-var Contact = {
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
-};
-
-var Address = {
-  fullAddress: function(){
-    return this.street + ', ' + this.city +', ' + this.state;
-  },
-  invalidAddress: function(){
-    if(this.street === '' || this.city === '' || this.state === ""){
-      return true;
-    } else {
-      return false;
+var ToDoList = {
+  addItem: function(item){
+    this.items.push(item)
     }
+};
+var ListOfLists = {
+  addList: function(list) {
+    this.lists.push(list)
   }
 };
-
-var PhoneNumber = {
-  invalidNumber: function() {
-    if(this.number.length < 10) {
-      return true;
-    } else {
-      return false;
-    }
-  },
-  listNumber: function(){
-        return this.number;
-  }
-};
-
 
 $(document).ready(function() {
-  var addressInvalid = false;
-  var numberInvalid = false;
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
+  var listToEdit;
+  var lols = Object.create(ListOfLists);
+  lols.lists = []
+  $('form#newToDoList').submit(function(event) {
+  event.preventDefault();
+  var newList = Object.create(ToDoList);
+  lols.addList(newList);
+  lols.lists[lols.lists.length-1].name = $('input#newList').val();
+  newList.items = [];
+  
+ 
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-
-    var newContact = Object.create(Contact);
-    newContact.firstName = inputtedFirstName;
-    newContact.lastName = inputtedLastName;
-
-    newContact.phoneNumbers = [];
-    newContact.addresses = [];
-
-    $(".new-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-
-      var newAddress = Object.create(Address);
-      newAddress.street = inputtedStreet;
-      newAddress.city = inputtedCity;
-      newAddress.state = inputtedState;
-      
-      
-      newContact.addresses.push(newAddress);
-    
+  $('#listOfLists ul').html('')
+  lols.lists.forEach(function(list){
+    $('#listOfLists ul').append('<li>' + list.name + '</li>');
+      $('#listOfLists ul li').last().click(function(){
+        $('h2.listToAddTo').html($(this).text()) 
+        listToEdit = list;
+        $('div.listItems ul').html('');
+        list.items.forEach(function(item, index){
+          $('div.listItems ul').append('<li>'+list.items[index]+'</li>')
+        });
+      })
     });
-
-    $('.new-phoneNumber').each(function(){
-      var inputtedNumber = $(this).find('.newNumber').val()
-      var newNumber = Object.create(PhoneNumber);
-      
-      newNumber.number = inputtedNumber;
-      newContact.phoneNumbers.push(newNumber);
-     
-      });
-
-   
-    numberInvalid =  newContact.phoneNumbers.some(function(element){
-     if (element.invalidNumber()){
-      return  true;
-    } else {
-      return false;
-    }
-    })
-    
-
-    addressInvalid = newContact.addresses.some(function(element) {
-      if (element.invalidAddress()){
-        return true;
-      } else {
-        return false;
-      }
-
-      console.log(addressInvalid)
-    })
-    
-    if ( !numberInvalid && !addressInvalid){
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-      
-      $("ul#phoneNumbers").text("");
-      newContact.phoneNumbers.forEach(function(number) {
-      $("ul#phoneNumbers").append("<li>" + number.listNumber() + "</li>");
-      });
-
-    });
-
-    this.reset();
-  } else {
-
-    alert('not valid info')
-  }
-  });
+  }); 
 
 
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                             '<div class="form-group">' +
-                               '<label for="new-street">Street</label>' +
-                               '<input type="text" class="form-control new-street">' +
-                             '</div>' +
-                             '<div class="form-group">' +
-                               '<label for="new-city">City</label>' +
-                               '<input type="text" class="form-control new-city">' +
-                              '</div>' +
-                              '<div class="form-group">' +
-                                '<label for="new-state">State</label>' +
-                                '<input type="text" class="form-control new-state">' +
-                              '</div>' +
-                              '</div>');
-    });
 
-  $("#add-number").click(function() {
-    $("#new-phone-numbers").append('<div class="new-phoneNumber">' +
-                                    '<div class="form-group">' +
-                                      '<label for="new-number">Phone number</label>' +
-                                      '<input type="text" class="form-control newNumber">' +
-                                    '</div>' +
-                                    '</div>');
-   });
+
+  $('form#newListItem').submit(function(event) {
+  event.preventDefault();
+  listToEdit.addItem( $('input#listItem').val());
+  
+ $('.listItems ul').html('')
+  listToEdit.items.forEach(function(item){
+    $('.listItems ul').append('<li>' + item + '</li>');
+  }); 
+ });
 });
-
